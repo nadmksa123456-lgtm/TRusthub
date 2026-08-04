c18c5dacfd6a47068bf3b5000db21574fdeff989
nadmksa123456-lgtm <nadmksa123456-lgtm@users.noreply.github.com>
2026-08-04 21:41:50 +0300

    Promote card depth v3 to stable v1.1.0

--- source.lua
+++ source.lua
@@ -119,8 +119,8 @@ local Fonts = {
 }
 
 local Library = {
-    Version = "1.1.0-dev",
-    Build = "design-card-depth-v3-right-corners-v1",
+    Version = "1.1.0",
+    Build = "stable-v1.1.0",
     FontName = "Roboto",
     FontScale = TYPOGRAPHY_SCALE,
     Theme = Theme,
